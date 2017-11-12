60daf2d1e17b5b505f70b2d72c18ce13246b4843
miazocool <zaimsaharudin@gmail.com>
2017-11-13 01:58:44 +0800

    Add files via upload
    testing budak baik

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Zaim Saharudin
+ *
+ * Created on November 13, 2017, 1:07 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    cout<< "hello world saya budak baik";
+    return 0;
+}
+
